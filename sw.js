d8208fdf9a6ab0ad4c804164d8bf1858db1190d4
ajh-001 <a.j.halcrow@gmail.com>
2026-05-16 23:01:25 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,53 +1,42 @@
-const CACHE_NAME = 'maverish-cache-v1';
+const CACHE_NAME = 'maverish-cache-v2';
 
-// We cache the app shell (HTML, manifest, and icons). 
-// External resources (like Leaflet scripts and map tiles) are usually excluded 
-// from basic caching to save device storage, but you can add them if you want strict offline support.
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
 ];
 
-// Install Event - caches the app shell
 self.addEventListener('install', event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch Event - serves from cache if available, otherwise goes to the network
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Activate Event - cleans up old caches
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
       );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
